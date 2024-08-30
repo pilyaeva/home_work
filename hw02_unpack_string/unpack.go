8bb8cc9149f79b61cddf325847fe6fce38834886
Polia Pilyaeva <pilyaevapolina@yandex.ru>
2024-08-30 14:58:39 +0300

    Complete homework

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,58 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(sourceString string) (string, error) {
+	resultStr := strings.Builder{}
+
+	previousChar := rune(0)
+	isPreviousSlash := false
+	isPreviousRealDigit := true
+
+	for _, currentChar := range sourceString {
+		if previousChar == '\\' && currentChar == '\\' && !isPreviousSlash {
+			isPreviousSlash = true
+			resultStr.WriteRune(previousChar)
+		} else if unicode.IsDigit(currentChar) && previousChar == '\\' && !isPreviousSlash {
+			resultStr.WriteRune(currentChar)
+			isPreviousRealDigit = false
+		} else {
+			if unicode.IsDigit(currentChar) {
+				if previousChar == 0 {
+					return "", ErrInvalidString
+				}
+
+				digit := currentChar - '0'
+
+				if isPreviousSlash || !isPreviousRealDigit {
+					digit--
+				}
+
+				if unicode.IsDigit(previousChar) && isPreviousRealDigit {
+					return "", ErrInvalidString
+				}
+
+				isPreviousRealDigit = true
+
+				resultStr.WriteString(strings.Repeat(string(previousChar), int(digit)))
+			} else if previousChar != 0 && !unicode.IsDigit(previousChar) && !isPreviousSlash {
+				resultStr.WriteRune(previousChar)
+			}
+
+			isPreviousSlash = false
+		}
+
+		previousChar = currentChar
+	}
+
+	if previousChar != 0 && !unicode.IsDigit(previousChar) {
+		resultStr.WriteRune(previousChar)
+	}
+
+	return resultStr.String(), nil
 }
