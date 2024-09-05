ceacea98acc66b2f47a99d1f812cd90dbf687473
Polia Pilyaeva <pilyaevapolina@yandex.ru>
2024-09-05 18:08:26 +0300

    The main part of the task.

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,58 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"regexp"
+	"sort"
+	"strings"
+)
+
+type kv struct {
+	Key   string
+	Value int
+}
+
+func Top10(sourceText string) []string {
+	re := regexp.MustCompile(`\s+`)
+	textWithoutExtraSpaces := re.ReplaceAllString(sourceText, " ")
+
+	if textWithoutExtraSpaces == "" {
+		return []string{}
+	}
+
+	words := strings.Split(textWithoutExtraSpaces, " ")
+	repeats := make(map[string]int)
+
+	for _, word := range words {
+		repeats[word]++
+	}
+
+	var sortedData []kv
+
+	for k, v := range repeats {
+		sortedData = append(sortedData, kv{Key: k, Value: v})
+	}
+
+	sort.Slice(sortedData, func(i, j int) bool {
+		if sortedData[i].Value == sortedData[j].Value {
+			return sortedData[i].Key < sortedData[j].Key
+		}
+
+		return sortedData[i].Value > sortedData[j].Value
+	})
+
+	var mostFrequencyWordsCount int
+
+	if len(sortedData) >= 10 {
+		mostFrequencyWordsCount = 10
+	} else {
+		mostFrequencyWordsCount = len(sortedData)
+	}
+
+	mostFrequencyWords := make([]string, mostFrequencyWordsCount)
+
+	for i := 0; i < mostFrequencyWordsCount; i++ {
+		mostFrequencyWords[i] = sortedData[i].Key
+	}
+
+	return mostFrequencyWords
 }
